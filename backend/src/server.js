17fe47101bfe242d25b54f45ade44977d3d09b1b
huydev14 <trgiahuy14@gmail.com>
2026-07-04 23:24:55 +0700

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+import authRoute from './routes/authRoute.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Middleware
+app.use(express.json());
+
+// public routes
+app.use('/api/auth', authRoute);
+
+// private routes
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
